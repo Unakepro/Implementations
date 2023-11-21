9429993b348ce3d5b69995abb8efc63faae02aca
Andrew <chebanov.andrew@gmail.com>
2023-11-21 16:21:12 +0100

    updated rest of methods

--- BigInt/BigInt.cpp
+++ BigInt/BigInt.cpp
@@ -3,24 +3,19 @@
 #include "BigInt.hpp"
 
 int main() {
-    BigInt x(10);
-    BigInt y(2);
+    BigInt x(-2);
+    BigInt y(11);
 
     std::cout << x << std::endl;
 	
-	//std::cin >> y;
-	//std::cout << y.toString() << std::endl;
-    //std::cin >> y;
-    //std::cout << y << std::endl;
+	std::cin >> y;
+	std::cout << y.toString() << std::endl;
 
 
-    //std::cout << (x*=y) << std::endl;
-    //std::cout << (x/=y) << std::endl;
-
-    //std::cout << x % x << std::endl;
+    std::cout << x % x << std::endl;
 
 
-	
+
     std::cout << (x > y) << std::endl;
     std::cout << (x < y) << std::endl;
     std::cout << (x >= y) << std::endl;
@@ -29,25 +24,20 @@ int main() {
     std::cout << (x == y) << std::endl;
     std::cout << (x != y) << std::endl;
 	
-	//std::cout << (-x) << std::endl;
+	std::cout << (-x) << std::endl;
 
-	for(int i=0; i<999999; ++i) {
-		BigInt k(i);
-		for(int j=0; j<999999;++j) {
-			BigInt v(j);
-			std::cout << "BigInt: " << (k+v) << " add: " << (i+j) << '\n';
-			assert(k+v == i+j);
-		}
-	}
 
-    //std::cout << (x+=y) << std::endl;
+    std::cout << (x+y) << std::endl;
+    std::cout << (x-y) << std::endl;
 
-    //std::cout << x++ << std::endl;
-    //std::cout << ++x << std::endl;
+    std::cout << (x*y) << std::endl;
+    std::cout << (x/y) << std::endl;
 
-    //std::cout << x-- << std::endl;
-    //std::cout << --x << std::endl;
+    std::cout << x++ << std::endl;
+    std::cout << ++x << std::endl;
 
+    std::cout << x-- << std::endl;
+    std::cout << --x << std::endl;
 
 
     return 0;
--- BigInt/BigInt.hpp
+++ BigInt/BigInt.hpp
@@ -17,13 +17,13 @@ public:
 		digits = std::to_string(x);
 	}
 
-    BigInt(const BigInt& obj): digits(obj.digits), sign(obj.sign) {}
+    BigInt(const BigInt& obj) = default;
 
 	BigInt() = default;
 
 	size_t size() const {
 		return digits.size();
-	} 
+	}
 
 	bool get_sign() const {
 		return sign;
@@ -43,37 +43,47 @@ public:
 	bool operator<(const BigInt&) const;
 	bool operator>=(const BigInt&) const;
 	bool operator<=(const BigInt&) const;
-	
+
 	bool operator==(const BigInt&) const;
 	bool operator!=(const BigInt&) const;
-	
+
 	BigInt& operator++();
 	BigInt& operator--();
-	BigInt operator++(int); 
+	BigInt operator++(int);
 	BigInt operator--(int);
 
-	BigInt operator-();
+	BigInt operator-() const;
 
 
 	char& operator[](int);
 	const char& operator[](int) const;
-	
+
 
 	friend std::ostream& operator<<(std::ostream& out, const BigInt& obj);
 	friend std::istream& operator>>(std::istream& in, BigInt& obj);
-	
+
 	explicit operator bool() {
 		return *this != 0;
-	}	
+	}
 
 };
+
+bool is_zero(std::string& obj) {
+    for(int i=obj.size()-1; i >= 0; --i) {
+        if(obj[i]-'0' != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 bool abs_compare(const BigInt& obj, const BigInt& obj1) {
 
 	if(obj.size() > obj1.size()) {
     	return true;
     }
 
-	if(obj.size() == obj1.size()) { 
+	if(obj.size() == obj1.size()) {
 		for(size_t i=0; i < obj.size(); ++i) {
             if((obj[i]-'0') > (obj1[i]-'0')) {
                 return true;
@@ -89,10 +99,10 @@ bool BigInt::operator>(const BigInt& obj) const {
     if(this->sign == 0 && obj.sign == 1) {
 		return true;
 	}
-	
+
 	if(this->sign == obj.sign) {
 		if(this->sign == 0) {
-			return abs_compare(*this, obj);	
+			return abs_compare(*this, obj);
 		}
 		else {
 			return abs_compare(obj, *this);
@@ -105,7 +115,7 @@ bool BigInt::operator<(const BigInt& obj) const {
 	return obj > (*this);
 }
 
-bool BigInt::operator==(const BigInt& obj) const {	
+bool BigInt::operator==(const BigInt& obj) const {
 	return !((*this) > obj || (*this) < obj);
 }
 
@@ -123,9 +133,10 @@ bool BigInt::operator<=(const BigInt& obj) const {
 }
 
 
-BigInt BigInt::operator-() {
-	sign = !sign;
-	return *this;
+BigInt BigInt::operator-() const {
+	BigInt copy(*this);
+	copy.sign = !copy.sign;
+    return copy;
 }
 
 
@@ -134,8 +145,10 @@ BigInt& BigInt::operator+=(const BigInt& obj) {
 
 	BigInt copy(obj);
 
-    if((*this).digits.size() < copy.digits.size()) {
+
+    if(abs_compare(copy, (*this))) {
 		std::swap(digits, copy.digits);
+        std::swap(sign, copy.sign);
     }
 
     size_t size_str = digits.size();
@@ -143,18 +156,18 @@ BigInt& BigInt::operator+=(const BigInt& obj) {
 
 
 	int carry=0;
-	if(sign == obj.sign) {
+	if(sign == copy.sign) {
 		for(size_t i = 0; i < cpy_size; ++i) {
 
 			carry += digits[size_str-1-i]-'0' + copy.digits[cpy_size-i-1]-'0';
             digits[size_str-1-i] = static_cast<char>(('0' + (carry % 10)));
 
 			carry /= 10;
-		}	
+		}
 
 		for(size_t j = cpy_size; j < size_str; ++j) {
 			carry += digits[size_str-1-j]-'0';
-			
+
 			digits[size_str-1-j] = static_cast<char>(('0' + (carry % 10)));
 
 			carry /= 10;
@@ -162,21 +175,53 @@ BigInt& BigInt::operator+=(const BigInt& obj) {
 
 		if(carry) {
 			digits = static_cast<char>(carry+'0') + digits;
-		
+
 		}
 	}
     else {
+        for(int i=0; i < cpy_size; ++i) {
+
+            if(digits[size_str-i-1]-'0' < copy.digits[cpy_size-i-1]-'0') {
+
+                int b_index = i+1;
+
+				while(digits[size_str-b_index-1]-'0' <= 0) {
+					++b_index;
+				}
+                digits[size_str-b_index-1] = static_cast<char>(((digits[size_str-b_index-1]-'0')-1)+'0');
+
+
+                for(int j = size_str-b_index; j < size_str-i-1; ++j) {
+					digits[j] = '9';
+				}
+
+				digits[size_str-i-1] = static_cast<char>(('0' + (10-(copy.digits[cpy_size-i-1]-'0')+(digits[size_str-i-1]-'0'))));
+				copy.digits[cpy_size-i-1] = 0;
+
+			} else {
+                digits[size_str-i-1] = static_cast<char>('0' + (digits[size_str-i-1]-'0') - (copy.digits[cpy_size-i-1]-'0'));
+            }
+        }
+
+
+        if(is_zero(digits)) {
+            sign = false;
+        }
+
+        while (digits.size() > 1 && digits[0] == '0') {
+            digits.erase(0, 1);
+        }
 
     }
 
 
-	return *this;
-					
+    return *this;
+
 }
 
-/*
+
 BigInt& BigInt::operator*=(const BigInt& obj) {
-	
+
 	BigInt copy = obj;
 	BigInt counter = (*this);
 
@@ -184,7 +229,7 @@ BigInt& BigInt::operator*=(const BigInt& obj) {
 		*this = 0;
 		return *this;
 	}
-	
+
 	if(copy > 0) {
 		while(copy != 1) {
 			(*this) += counter;
@@ -208,9 +253,9 @@ BigInt& BigInt::operator*=(const BigInt& obj) {
 BigInt& BigInt::operator/=(const BigInt& obj) {
 
 	if(obj == 0) {
-		throw "division by 0";
+		throw std::runtime_error("division by 0");
 	}
-	
+
 
 	BigInt result(0);
 	bool res_sign = (sign ^ obj.sign);
@@ -222,8 +267,8 @@ BigInt& BigInt::operator/=(const BigInt& obj) {
 	while(*this >= abs_obj) {
 		++result;
 		*this -= abs_obj;
-	}	
-	
+	}
+
 	*this = result;
 
 	if(this == 0) {
@@ -236,20 +281,18 @@ BigInt& BigInt::operator/=(const BigInt& obj) {
 	return *this;
 }
 
-
-
 BigInt& BigInt::operator-=(const BigInt& obj) {
-	-(*this) += (obj);
-	return (-*this);
+    BigInt copy = obj;
+	(*this) += (-obj);
+	return (*this);
 }
 
-*/
 BigInt operator+(const BigInt& obj, const BigInt& obj1) {
 	BigInt copy = obj;
 	copy += obj1;
 	return copy;
 }
-/*
+
 BigInt operator-(const BigInt& obj, const BigInt& obj1) {
     BigInt copy = obj;
 	copy -= obj1;
@@ -298,10 +341,6 @@ BigInt& BigInt::operator--() {
     return *this;
 }
 
-
-
-*/
-
 char& BigInt::operator[](int x) {
 	return digits[x];
 }
@@ -311,24 +350,24 @@ const char& BigInt::operator[](int x) const {
 }
 
 std::ostream& operator<<(std::ostream& out, const BigInt& obj) {
-	
+
 	if(obj.sign == 1) {
 		out << '-';
 	}
 	out << obj.digits;
-	
+
 	return out;
 }
 
 std::istream& operator>>(std::istream& in, BigInt& obj) {
 
 	std::string str;
-	
+
 	in >> str;
-	
+
 	if(str[0] == '-') {
 		obj.sign = true;
-		obj.digits = str.substr(1);	
+		obj.digits = str.substr(1);
 	}
 	else {
 		obj.sign = false;
